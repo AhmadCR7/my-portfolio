0e3b8c6b90371d0107d0624b1cffb4c9025c07f6
Ahmad Afghan <afghan.ahmadmujtaba@gmail.com>
2023-09-06 15:18:51 -0700

    fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, Nav, Container, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../style.css";
